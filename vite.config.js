1fbbef7262de5278a92c6ad55d24c663f49b6f06
Fernando Pérez Mojica <107861444+fermop@users.noreply.github.com>
2025-03-28 06:57:39 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicPath: process.env.NODE_ENV === "production" ? "/guitarla-vue/" : "/",
+  base: '/guitarla-vue/',
 })
